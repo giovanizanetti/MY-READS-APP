5405cd244cd9fdbb8a9e1432999dd7b1fb9e680d
giovanizanetti <zanetti.giovani@gmail.com>
2020-12-26 14:22:53 +0100

    test: add navigation test

---
+++ src/components/Navigation/navigation.test.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Navigation from './Navigation'
+
+it('renders Navigation component', () => {
+  const div = document.createElement('div')
+  ReactDOM.render(<Navigation />, div)
+})
