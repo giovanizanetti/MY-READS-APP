5d5d375aa08afaf8d04373ff65fe67e80342838b
giovanizanetti <zanetti.giovani@gmail.com>
2020-12-26 23:46:12 +0100

    feat: displays books dinamicaly

--- src/components/Book/Book.js
+++ src/components/Book/Book.js
@@ -1,4 +1,12 @@
-const Book = (book) => {
+const Book = ({ book }) => {
+  const {
+    imageLinks: { thumbnail },
+    title,
+    authors,
+  } = book
+
+  const bookAuthors = authors && authors.map((author) => <p>{author}</p>)
+
   return (
     <li>
       <div className='book'>
@@ -8,8 +16,7 @@ const Book = (book) => {
             style={{
               width: 128,
               height: 192,
-              backgroundImage:
-                'url("http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api")',
+              backgroundImage: `url(${thumbnail})`,
             }}
           ></div>
           <div className='book-shelf-changer'>
@@ -24,8 +31,8 @@ const Book = (book) => {
             </select>
           </div>
         </div>
-        <div className='book-title'>The Hobbit</div>
-        <div className='book-authors'>J.R.R. Tolkien</div>
+        <div className='book-title'>{title}</div>
+        <div className='book-authors'>{bookAuthors}</div>
       </div>
     </li>
   )
--- src/components/BookList/BookList.js
+++
@@ -1,7 +0,0 @@
-import Book from '../Book/Book'
-
-const BookList = (books) => {
-  return <ol className='books-grid'>{books && books.map((book) => <Book book={book} />)}</ol>
-}
-
-export default BookList
