ddc1e0f287074f927df3f549b291f20fe7edb26a
giovanizanetti <zanetti.giovani@gmail.com>
2020-12-28 11:07:48 +0100

    chore: create BooksProvider

--- src/App.js
+++ src/App.js
@@ -1,27 +1,35 @@
+import { useContext } from 'react'
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import './App.css'
 import Navigation from './components/Navigation/Navigation'
 import Main from './components/Main/Main'
 import Search from './components/Search/Search'
+import Store from './Store'
+import { Context } from './Store'
 
 const BooksApp = () => {
+  const bla = useContext(Context)
+
+  console.log(bla && bla, 'bla')
   return (
     <div className='app'>
       <Navigation />
       <Router>
-        <Switch>
-          <Route exact path='/'>
-            <div className='list-books'>
-              <Main />
-              <Link to='/search' className='open-search'>
-                <button>Add a book</button>
-              </Link>
-            </div>
-          </Route>
-          <Route path='/search'>
-            <Search />
-          </Route>
-        </Switch>
+        <Store>
+          <Switch>
+            <Route exact path='/'>
+              <div className='list-books'>
+                <Main />
+                <Link to='/search' className='open-search'>
+                  <button>Add a book</button>
+                </Link>
+              </div>
+            </Route>
+            <Route path='/search'>
+              <Search />
+            </Route>
+          </Switch>
+        </Store>
       </Router>
     </div>
   )
