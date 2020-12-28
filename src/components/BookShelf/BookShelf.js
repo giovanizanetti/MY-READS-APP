885435aeb9fe657a82081384def81ce4180e6d3f
giovanizanetti <zanetti.giovani@gmail.com>
2020-12-28 11:43:26 +0100

    refactor: handleShelf
    pass handleShelf down though the BooksContext as opposed to pass it five layers down via props. Therefore wirte cleaner code.

--- src/components/BookShelf/BookShelf.js
+++ src/components/BookShelf/BookShelf.js
@@ -1,13 +1,11 @@
 import Book from '../Book/Book'
 
-const BookShelf = ({ name, books, handleShelf }) => {
+const BookShelf = ({ name, books }) => {
   return (
     <section className='bookshelf'>
       <h2 className='bookshelf-title'>{name}</h2>
       <div className='bookshelf-books'>
-        <ol className='books-grid'>
-          {books && books.map((book) => <Book key={book.id} book={book} handleShelf={handleShelf} />)}
-        </ol>
+        <ol className='books-grid'>{books && books.map((book) => <Book key={book.id} book={book} />)}</ol>
       </div>
     </section>
   )
