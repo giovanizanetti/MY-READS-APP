da38302307265972398f940911337fc08409a373
giovanizanetti <zanetti.giovani@gmail.com>
2020-12-29 12:40:28 +0100

    fix: update my shelfs in real time when new booked is added

--- src/App.js
+++ src/App.js
@@ -4,22 +4,28 @@ import Navigation from './components/Navigation/Navigation'
 import Main from './components/Main/Main'
 import { BooksProvider } from './BooksProvider'
 import { getAll, update } from './BooksAPI'
-import { useHistory } from 'react-router-dom'
-console.log(useHistory)
 
 const BooksApp = () => {
   const [searchResults, setSearchResults] = useState([])
   const [books, setBooks] = useState([])
+  const [shouldUpdate, setShouldUpdate] = useState(true)
 
   const currentlyReading = books && books.filter((book) => book.shelf === 'currentlyReading')
   const wantToRead = books && books.filter((book) => book.shelf === 'wantToRead')
   const read = books && books.filter((book) => book.shelf === 'read')
 
   useEffect(() => {
-    getAll().then((data) => {
-      setBooks(data)
-    })
-  }, [])
+    console.log('n')
+    if (shouldUpdate) {
+      getAll()
+        .then((data) => {
+          setBooks(data)
+        })
+        .then(() => setShouldUpdate(false))
+        .catch((err) => console.log(err))
+    }
+    setShouldUpdate(false)
+  }, [shouldUpdate])
 
   const handleShelf = (shelf, book) => {
     const { id } = book
@@ -39,6 +45,7 @@ const BooksApp = () => {
       // When book does not exists, add it to the selected shelf
     } else {
       setBooks((prevBooks) => [...prevBooks, book])
+      setShouldUpdate(true)
       // setSelectedBook(book)
     }
   }
@@ -50,6 +57,7 @@ const BooksApp = () => {
       {/* Pass books down to make it available for any consumer component within the provider */}
       <BooksProvider
         value={{
+          // @ts-ignore
           handleShelf,
           searchResults,
           setSearchResults,
