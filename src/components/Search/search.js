6e1b2a7b63b2bd5f38044db86862a28ac57b798e
giovanizanetti <zanetti.giovani@gmail.com>
2020-12-29 19:50:31 +0100

    improvement: useDebounce
    create useDebounce hook to abstracthe logic, tehreferofe could be re-used in different components

--- src/components/Search/search.js
+++ src/components/Search/search.js
@@ -2,26 +2,42 @@ import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import BookList from '../BookList/BookList'
 import { search } from '../../BooksAPI'
+import { useDebounce } from '../../hooks/useDebounce'
 
 const Search = () => {
+  // APi query value (delayed)
   const [query, setQuery] = useState('')
   const [searchResults, setSearchResults] = useState([])
   const [message, setMessage] = useState('')
+  // Inpute value
+  const [value, setValue] = useState('')
 
   useEffect(() => {
+    if (!query) {
+      setSearchResults([])
+      setMessage('')
+    }
     query && search(query).then((data) => setSearchResults(data))
   }, [query])
 
-  const handleChange = (e) => {
-    setMessage('')
-    const searchTerm = e.target.value.toLowerCase()
-    if (!searchTerm) setSearchResults([])
-    setQuery(searchTerm)
-
+  useEffect(() => {
     if (query.length && !searchResults.length) {
-      console.log('vvv')
+      setTimeout(() => {
+        setMessage('Not found! Try another search term!')
+      }, 700)
     }
+  }, [query, searchResults])
+
+  const debouncedSave = useDebounce((value) => setQuery(value), 500)
+
+  const handleChange = (e) => {
+    const text = e.target.value.toLowerCase()
+    // input value to display immediately
+    setValue(text)
+    // delay query to deminish api calls
+    debouncedSave(text)
   }
+
   return (
     <div className='search-books'>
       <div className='search-books-bar'>
@@ -29,7 +45,7 @@ const Search = () => {
           Close
         </Link>
         <div className='search-books-input-wrapper'>
-          <input type='text' placeholder='Search by title or author' onChange={handleChange} value={query} />
+          <input type='text' placeholder='Search by title or author' onChange={handleChange} value={value} />
         </div>
       </div>
       <div className='search-books-results'>
