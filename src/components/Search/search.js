b24cd19a3f780246dd4160a9e7bc9bd02797d911
giovanizanetti <zanetti.giovani@gmail.com>
2020-12-29 19:50:31 +0100

    improvement: useDebounce
    create useDebounce hook to abstractthe logict for debouncing

--- src/components/Search/search.js
+++ src/components/Search/search.js
@@ -2,26 +2,35 @@ import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import BookList from '../BookList/BookList'
 import { search } from '../../BooksAPI'
+import { useDebounce } from '../../hooks/useDebounce'
 
 const Search = () => {
   const [query, setQuery] = useState('')
   const [searchResults, setSearchResults] = useState([])
   const [message, setMessage] = useState('')
+  const [value, setValue] = useState('')
 
   useEffect(() => {
+    console.log('calAPI')
+    if (!query) {
+      setSearchResults([])
+      setMessage('')
+    }
     query && search(query).then((data) => setSearchResults(data))
   }, [query])
 
+  const debouncedSave = useDebounce((value) => setQuery(value), 500)
+
   const handleChange = (e) => {
     setMessage('')
-    const searchTerm = e.target.value.toLowerCase()
-    if (!searchTerm) setSearchResults([])
-    setQuery(searchTerm)
-
-    if (query.length && !searchResults.length) {
-      console.log('vvv')
+    const text = e.target.value.toLowerCase()
+    setValue(text)
+    debouncedSave(text, 500)
+    if (value.length && !searchResults.length) {
+      setMessage('Not found! Try another search term!')
     }
   }
+
   return (
     <div className='search-books'>
       <div className='search-books-bar'>
@@ -29,7 +38,7 @@ const Search = () => {
           Close
         </Link>
         <div className='search-books-input-wrapper'>
-          <input type='text' placeholder='Search by title or author' onChange={handleChange} value={query} />
+          <input type='text' placeholder='Search by title or author' onChange={handleChange} value={value} />
         </div>
       </div>
       <div className='search-books-results'>
