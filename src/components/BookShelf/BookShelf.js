ee597ea7dcf0646c8b3f2069a220f95772ab0dc9
giovanizanetti <zanetti.giovani@gmail.com>
2020-12-30 18:28:25 +0100

    feat: empty shelf
    enable the user to empty each shelf individualy

--- src/components/BookShelf/BookShelf.js
+++ src/components/BookShelf/BookShelf.js
@@ -1,16 +1,37 @@
 import { useContext } from 'react'
 import { StoreContext } from '../../Store'
 import BookList from '../BookList/BookList'
+import icon from '../../icons/delete.svg'
+import { update } from '../../BooksAPI'
 
 const BookShelf = ({ name, books }) => {
-  const { darkTheme } = useContext(StoreContext)
-
+  const { darkTheme, currentlyReading, read, wantToRead, setBooks } = useContext(StoreContext)
   const darkStyle = { color: '#7dad7b', borderBottomColor: 'rgb(97 92 92' }
+
+  const handleDelete = () => {
+    if (name === 'Currently Reading') {
+      setBooks([...wantToRead, ...read])
+    }
+    if (name === 'Want to Read') {
+      setBooks([...currentlyReading, ...read])
+    }
+    if (name === 'Read') {
+      setBooks([...currentlyReading, ...wantToRead])
+    }
+
+    books.map((book) => update(book.id, 'none'))
+    console.log(name)
+  }
+
   return (
     <section className='bookshelf'>
-      <h2 style={darkTheme ? darkStyle : null} className='bookshelf-title'>
-        {name && name}
-      </h2>
+      <div>
+        <h2 style={darkTheme ? darkStyle : null} className='bookshelf-title'>
+          {name && name}
+        </h2>
+        {books.length ? <img onClick={handleDelete} width='26px' src={icon} alt='empty shelf' /> : ''}
+      </div>
+
       <div className='bookshelf-books'>
         <BookList books={books} />
       </div>
