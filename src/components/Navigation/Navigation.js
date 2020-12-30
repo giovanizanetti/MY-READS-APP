1fceb124731b76b0f475629dbaf91784560c05a4
giovanizanetti <zanetti.giovani@gmail.com>
2020-12-30 13:17:44 +0100

    feat: create and style darkTheme handler

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,7 +1,20 @@
+import { dark } from '@material-ui/core/styles/createPalette'
+import { useContext } from 'react'
+import { StoreContext } from '../../Store'
+
 const Navigation = () => {
+  const { darkTheme, setDarkTheme } = useContext(StoreContext)
+  const buttonText = darkTheme ? 'Switch to light theme' : 'Switch to dark theme'
+
+  const style = { display: 'flex', justifyContent: 'space-between', padding: '10px 30px' }
+  const buttonLightStyle = { background: '#313131', color: '#f2f2f2' }
+
   return (
-    <nav className='list-books-title'>
+    <nav style={style} className='list-books-title'>
       <h1>MyReads</h1>
+      <button style={!darkTheme ? buttonLightStyle : null} onClick={() => setDarkTheme(!darkTheme)}>
+        {buttonText}
+      </button>
     </nav>
   )
 }
