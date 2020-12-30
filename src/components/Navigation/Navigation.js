e545f359c7a252b27fcd5dc1ed0b7a1823561601
giovanizanetti <zanetti.giovani@gmail.com>
2020-12-30 13:20:49 +0100

    improvement: blur theme handler on click

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -7,12 +7,17 @@ const Navigation = () => {
   const buttonText = darkTheme ? 'Switch to light theme' : 'Switch to dark theme'
 
   const style = { display: 'flex', justifyContent: 'space-between', padding: '10px 30px' }
-  const buttonLightStyle = { background: '#313131', color: '#f2f2f2' }
+  const buttonLightStyle = { background: '#554e4e', color: '#f2f2f2' }
+
+  const handleClick = (e) => {
+    e.target.blur()
+    setDarkTheme(!darkTheme)
+  }
 
   return (
     <nav style={style} className='list-books-title'>
       <h1>MyReads</h1>
-      <button style={!darkTheme ? buttonLightStyle : null} onClick={() => setDarkTheme(!darkTheme)}>
+      <button style={!darkTheme ? buttonLightStyle : null} onClick={handleClick}>
         {buttonText}
       </button>
     </nav>
