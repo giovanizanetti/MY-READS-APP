2282645e21c1ac96ecb17a892d08b38cbaa57ff6
giovanizanetti <zanetti.giovani@gmail.com>
2020-12-30 12:16:00 +0100

    feat: apply dark theme on state change to BookShelf compoenent

--- src/components/BookShelf/BookShelf.js
+++ src/components/BookShelf/BookShelf.js
@@ -5,7 +5,7 @@ import BookList from '../BookList/BookList'
 const BookShelf = ({ name, books }) => {
   const { darkTheme } = useContext(StoreContext)
 
-  const darkStyle = { color: '#7dad7b' }
+  const darkStyle = { color: '#7dad7b', borderBottomColor: 'rgb(97 92 92' }
   return (
     <section className='bookshelf'>
       <h2 style={darkTheme ? darkStyle : null} className='bookshelf-title'>
